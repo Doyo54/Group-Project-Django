d60459bd3326222dfa5e0483eab89e3df79f6a0f
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-06-22 09:36:55 +0300

    ft: Post model class created

--- doyo/models.py
+++ doyo/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=60)
+    description = models.TextField()
+    # user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='posts', null=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+  
+
+    @classmethod
+    def get_Profile(cls, user):
+        profile = Post.objects.filter(user__user=user).all()
+        return profile
+
+    def save_post(self):
+        self.save()
+    
+    @classmethod
+    def update_post(cls, id, value):
+        cls.objects.filter(id=id).update(title=value)
+
+    @classmethod
+    def update_caption(cls, id, value):
+        cls.objects.filter(id=id).update(description=value)
+
+    def delete_image(self):
+        self.delete()
+
+    def __str__(self):
+        return self.title
