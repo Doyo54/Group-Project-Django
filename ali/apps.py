a70df3b871cd9681f88c33a1a6ad9553075ff1eb
Benie <davinci.monalissa@gmail.com>
2022-06-22 10:56:10 +0300

    update models--create a doctor account manager & model class

--- ali/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AliConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ali'
--- benjamin/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BenjaminConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'benjamin'
