e7877b576a296b0a4aea34bf7efede5237d81906
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-06-23 13:00:53 +0300

    install whitenoise and bootstrap4

--- app/settings.py
+++ app/settings.py
@@ -40,14 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cindy',
     'benjamin',
-    'bootstrap4'
+    'bootstrap4',
     'fontawesomefree',
 
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
